fc6138308212920d98441e0b68728c2b525f9c9a
SIPHAMANDLA MEHLOMAKULU <mr.smehlomakulu@gmail.com>
2021-08-13 02:25:04 +0200

    Add getMembers function to the api

---
+++ src/lib/api/members.js
@@ -0,0 +1,27 @@
+import { fetchData } from "./api";
+
+// Get all events
+export const getMembers = async () => {
+    const query = `
+    query MyQuery {
+        members {
+          nodes {
+            id
+            memberDetails {
+              names
+              surname
+              position
+              profilePicture {
+                id
+                sourceUrl
+                altText
+              }
+            }
+            
+          }
+        }
+      }
+      `;
+    const data = await fetchData(query)
+    return data;
+}
