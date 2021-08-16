40f085c6764306c2a08d6ba000ca8c1b23ed5ac7
SIPHAMANDLA MEHLOMAKULU <mr.smehlomakulu@gmail.com>
2021-08-16 05:14:08 +0200

    Add horizontal padding to the container element

--- src/UI/Container.js
+++ src/UI/Container.js
@@ -5,6 +5,7 @@ export const Container = ({ children }) => {
         maxWidth: "1200px",
         width: "100%",
         margin: "0 auto",
+        padding: "0 1em"
     }
     return (
         <div style={style}>
