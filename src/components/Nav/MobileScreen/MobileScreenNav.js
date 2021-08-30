2140a4ee8d787e13fa74a5fa9f5ae072e1411674
SIPHAMANDLA MEHLOMAKULU <mr.smehlomakulu@gmail.com>
2021-08-30 05:25:56 +0200

    improve mobile navigation

--- src/components/Nav/MobileScreen/MobileScreenNav.js
+++ src/components/Nav/MobileScreen/MobileScreenNav.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import styles from './MobileScreenNav.module.scss';
 import { NavLink } from 'react-router-dom';
+import rotary_logo from '../../../assets/images/rotary_logo.svg';
 
 const Nav = () => {
 
@@ -8,22 +9,25 @@ const Nav = () => {
 
     return (
         <div className={styles.Nav}>
+            <div className={styles.LogoContainer}>
+                <img src={rotary_logo} alt="Rotary Logo" /> <h1>Port Alfred</h1>
+            </div>
             <button onClick={() => setOpen(!open)}>Menu</button>
             <ul style={{ display: open ? 'block' : 'none' }}>
                 <li>
-                    <NavLink to="/" activeClassName={styles.Active} exact>About Rotary</NavLink>
+                    <NavLink onClick={() => setOpen(!open)} to="/" activeClassName={styles.Active} exact>About Rotary</NavLink>
                 </li>
                 <li>
-                    <NavLink to="/causes" activeClassName={styles.Active} >Our causes</NavLink>
+                    <NavLink onClick={() => setOpen(!open)} to="/causes" activeClassName={styles.Active} >Our causes</NavLink>
                 </li>
                 <li>
-                    <NavLink to="/events" activeClassName={styles.Active} >Events</NavLink>
+                    <NavLink onClick={() => setOpen(!open)} to="/events" activeClassName={styles.Active} >Events</NavLink>
                 </li>
                 <li>
-                    <NavLink to="/news" activeClassName={styles.Active} >News</NavLink>
+                    <NavLink onClick={() => setOpen(!open)} to="/news" activeClassName={styles.Active} >News</NavLink>
                 </li>
                 <li>
-                    <NavLink to="/Members" activeClassName={styles.Active} >Members</NavLink>
+                    <NavLink onClick={() => setOpen(!open)} to="/Members" activeClassName={styles.Active} >Members</NavLink>
                 </li>
             </ul>
         </div>
